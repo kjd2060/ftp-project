5bde30a36ec72ebb36799d8677a1f5e7a9d0e126
kjd2060 <gamelord317@gmail.com>
2018-05-01 16:36:55 -0400

    removed date/time stuff, started implementing ftp stuff
    Have the skeleton for the ftp stuff there, need to implement the actual functionality.  UI basically done at this point.

--- ftp-client/src/com/kjd/Client.java
+++ ftp-client/src/com/kjd/Client.java
@@ -8,42 +8,90 @@ public class Client {
     private Socket socket = null;
     private DataInputStream input = null;
     private DataOutputStream out = null;
+    boolean connected = false;
+
+    final String loginGood = "Authenticated.  Logging you in.";
 
     public Client() {
         try{
             Scanner scn = new Scanner(System.in);
 
             // get localhost ip
-            InetAddress ip = InetAddress.getByName("localhost");
+            InetAddress ip = null;
 
-            Socket s = new Socket(ip, 5000);
+            Socket s = null;
 
             // obtain in / out
-            DataInputStream in = new DataInputStream(s.getInputStream());
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            DataInputStream in = null;
+            DataOutputStream out = null;
 
             // perform info exchange between client + handler
             while(true){
-                System.out.println(in.readUTF());
-                String tosend = scn.nextLine();
-                out.writeUTF(tosend);
+                if(connected){
+                    System.out.print(in.readUTF());
+                    String tosend = scn.nextLine();
+                    out.writeUTF(tosend);
 
-                // if client sends exit, close connection + break from loop
-                if(tosend.equals("Exit")){
-                    System.out.println("Closing connection: " + s);
-                    s.close();
-                    System.out.println("Connection closed");
-                    break;
+                    // if client sends exit, close connection + break from loop
+                    if(tosend.equals("Exit")){
+                        System.out.println("Closing connection: " + s);
+                        s.close();
+                        System.out.println("Connection closed");
+                        break;
+                    }
+
+                    String received = in.readUTF();
+                    System.out.println(received);
                 }
-
-                String received = in.readUTF();
-                System.out.println(received);
+                else{
+                    String tosend = scn.nextLine();
+                    if(tosend.equals("Exit")){
+                        System.out.println("Exiting!");
+                        break;
+                    }
+                    else{
+                        String[] splited = tosend.split("\\s+");
+                        if(splited[0].equals("rftp")){
+                            if(splited.length < 4){
+                                System.out.println("Usage: rftp<server addr><username><password>");
+                                continue;
+                            }
+                            else{
+                                String serverAddr = splited[1];
+                                String uname = splited[2];
+                                String password = splited[3];
+                                ip = InetAddress.getByName(serverAddr);
+                                s = new Socket(ip, 5000);
+                                in = new DataInputStream(s.getInputStream());
+                                out = new DataOutputStream(s.getOutputStream());
+                                out.writeUTF(tosend);
+                                String response = in.readUTF();
+                                System.out.println(response);
+                                if(response.equals(loginGood)){
+                                    connected = true;
+                                }
+                                else{
+                                    connected = false;
+                                }
+                            }
+                        }
+                        else{
+                            System.out.println("Commands: rftp <server addr><username><password>, Exit");
+                        }
+                    }
+                }
             }
 
             // close resources
-            scn.close();
-            in.close();
-            out.close();
+            if(scn != null){
+                scn.close();
+            }
+            if(in != null){
+                in.close();
+            }
+            if(out != null){
+                out.close();
+            }
         } catch(Exception e){
             e.printStackTrace();
         }
--- ftp-server/src/com/kjd/ClientHandler.java
+++ ftp-server/src/com/kjd/ClientHandler.java
@@ -28,39 +28,122 @@ public class ClientHandler extends Thread {
     public void run(){
         String received;
         String toreturn;
+        boolean authenticated = false;
+
         while(true) {
             try {
-                // ask user what want to do
-                out.writeUTF("What do you want?;[Data | Time]..\n" +
-                        "Type Exit to terminate connection.");
 
-                // receive anser from client
-                received = in.readUTF();
+                if(!authenticated){
+                    // receive anser from client
+                    received = in.readUTF();
 
-                if (received.equals("Exit")) {
-                    System.out.println("Client " + this.s + " sends exit...");
-                    System.out.println("Closing this connection.");
-                    this.s.close();
-                    System.out.println("Connection closed.");
-                    break;
+                    String[] splited = received.split("\\s+");
+
+                    // write on output based on answer from client
+                    String filename;
+                    switch(splited[0]){
+                        case "rftp":
+                            toreturn = "";
+                            if(splited.length < 4){
+                                toreturn = "Usage: rftp <FTP Server Address><Username><Password>";
+                            }
+                            else{
+                                String serverAddr = splited[1];
+                                String user = splited[2];
+                                String password = splited[3];
+
+                                FileReader fileReader = new FileReader("users.txt");
+                                BufferedReader fileBuffer = new BufferedReader(fileReader);
+
+                                String line = "";
+                                while((line = fileBuffer.readLine()) != null){
+                                    if(line.contains("//")){
+                                        // ignore line
+                                        continue;
+                                    }
+                                    else {
+                                        String[] arr = line.split(",");
+                                        String uname = arr[0].split(":")[1];
+                                        uname = uname.replaceAll("\\s+", ""); // replace whitespace
+                                        String pw = arr[1].split(":")[1];
+                                        pw = pw.replaceAll("\\s+", "");
+                                        System.out.println(uname + ", " + pw);
+                                        if(uname.equals(user) && pw.equals(password)){
+                                            authenticated = true;
+                                            toreturn = "Authenticated.  Logging you in.";
+                                        }
+                                        else{
+                                            toreturn = "Invalid username or password";
+                                            authenticated = false;
+                                            out.writeUTF(toreturn);
+                                            s.close();
+                                        }
+                                    }
+                                }
+                            }
+                            out.writeUTF(toreturn);
+                            break;
+                        default:
+                            toreturn = "Please log in to use features.";
+                            out.writeUTF(toreturn);
+                            break;
+                    }
                 }
+                else {
+                    // ask user what want to do
+                    out.writeUTF("~");
 
-                // creatin date object
-                Date date = new Date();
+                    // receive anser from client
+                    received = in.readUTF();
 
-                // write on output based on answer from client
-                switch (received) {
-                    case "Date":
-                        toreturn = fordate.format(date);
-                        out.writeUTF(toreturn);
+                    String[] splited = received.split("\\s+");
+
+                    // write on output based on answer from client
+                    String filename;
+                    if (received.equals("Exit")) {
+                        System.out.println("Client " + this.s + " sends exit...");
+                        System.out.println("Closing this connection.");
+                        this.s.close();
+                        System.out.println("Connection closed.");
                         break;
-                    case "Time":
-                        toreturn = fortime.format(date);
-                        out.writeUTF(toreturn);
-                        break;
-                    default:
-                        out.writeUTF("Invalid input");
-                        break;
+                    }
+
+                    switch (splited[0]) {
+                        case "ls":
+                            toreturn = "list the files in the directory here";
+                            out.writeUTF(toreturn);
+                            break;
+                        case "cd":
+                            if (splited.length < 2) {
+                                toreturn = "Usage: cd <directory name>";
+                            } else {
+                                String directoryDest = splited[1];
+                                toreturn = directoryDest + "/";
+                            }
+                            out.writeUTF(toreturn);
+                            break;
+                        case "rget":
+                            toreturn = "";
+                            if (splited.length < 2) {
+                                toreturn = "Usage: rget <filename>";
+                            } else {
+                                filename = splited[1];
+                            }
+                            out.writeUTF(toreturn);
+                            break;
+                        case "rput":
+                            toreturn = "";
+                            if (splited.length < 2) {
+                                toreturn = "Usage: rput <filename>";
+                            } else {
+                                filename = splited[1];
+                            }
+                            out.writeUTF(toreturn);
+                            break;
+                        default:
+                            out.writeUTF("Invalid input");
+                            break;
+                    }
                 }
             } catch (SocketException s){
                 System.out.println(s);
